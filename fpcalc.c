9ee586add6a173939bcc93fddbb51fde34238c64
Kaanthi Pandhigunta <kpandhigunta@g.hmc.edu>
2023-04-09 10:57:01 -0700

    debug 64 bit attempts

--- fpcalc.c
+++ fpcalc.c
@@ -31,7 +31,7 @@ int opSize = 0;
 
 void long2binstr(unsigned long  val, char *str, int bits) {
   int i, shamt;
-  unsigned long mask, masked;
+  unsigned long long mask, masked;
 
   if (val == 0) { // just return zero
     str[0] = '0';
@@ -108,7 +108,7 @@ void printF32(char *msg, float32_t f) {
 
 void printF64(char *msg, float64_t f) {
   dp conv;
-  long exp, fract;
+  long long exp, fract;
   long long mask;
   char sign;
   char sci[200], fractstr[200];
@@ -119,12 +119,12 @@ void printF64(char *msg, float64_t f) {
   fract = f.v & mask; long2binstr(fract, fractstr, 52);
   exp = (f.v >> 52) & ((1<<11) -1);
   sign = f.v >> 63 ? '-' : '+';
-  //printf("%c %d %d  ", sign, exp, fract);
+  printf("\nshrek %c %lld %lld  \n", sign, exp, fract);
   if (exp == 0 && fract == 0) sprintf(sci, "%czero", sign);
   else if (exp == 0 && fract != 0) sprintf(sci, "Denorm: %c0.%s x 2^-1022", sign, fractstr);
   else if (exp == 2047 && fract == 0) sprintf(sci, "%cinf", sign);
   else if (exp == 2047 && fract != 0) sprintf(sci, "NaN Payload: %c%s", sign, fractstr);
-  else sprintf(sci, "%c1.%s x 2^%ld", sign, fractstr, exp-1023);
+  else sprintf(sci, "%c1.%s x 2^%lld", sign, fractstr, exp-1023);
 
   //printf ("%s: 0x%016lx = %lg\n", msg, conv.v, conv.d);
   printf("%s: ", msg);
@@ -135,7 +135,7 @@ void printF64(char *msg, float64_t f) {
   printf("%04llx", (conv.v >> 16) & 0xFFFF);
   printf("_");  
   printf("%04llx", (conv.v & 0xFFFF));
-  printf(" = %lg = %s: Biased Exp %ld Fract 0x%lx\n", conv.d, sci, exp, fract);
+  printf(" = %lg = %s: Biased Exp %lld Fract 0x%llx\n", conv.d, sci, exp, fract);
   //printf ("%s: 0x%016lx = %lg = %s: Biased Exp %d Fract 0x%lx\n", 
   //  msg, conv.v, conv.d, sci, exp, fract); 
 }
@@ -206,6 +206,7 @@ char parseRound(char *rnd) {
 
 int main(int argc, char *argv[])
 {
+    opSize = 0;
     uint64_t xn, yn, zn;
     char op1, op2;
     char cmd[200];
